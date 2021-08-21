230c0698563ab6724e5046bb99a657dde1ba8b09
arthaputra <manager.arthachan@gmail.com>
2021-08-21 18:10:48 +0800

    export

--- app/Exports/data_pesertaExport.php
+++ app/Exports/data_pesertaExport.php
@@ -2,12 +2,16 @@
 
 namespace App\Exports;
 
+use Carbon\Carbon;
 use App\Models\data_peserta;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class data_pesertaExport implements FromView, ShouldAutoSize
+class data_pesertaExport implements FromCollection, ShouldAutoSize, WithMapping, WithHeadings
 {
 
   public $id;
@@ -15,15 +19,41 @@ class data_pesertaExport implements FromView, ShouldAutoSize
   {
     $this->id = $id;
   }
+    // public function view(): View
+    // {
+    //     return view('pesertaExport',[
+    //     'data_peserta'=>data_peserta::where('competition_id', $this->id)->get()
+    //   ]);
 
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function view(): View
+    // }
+    public function collection()
     {
-        return view('pesertaExport',[
-        'data_peserta'=>data_peserta::where('competition_id', $this->id)->get()
-      ]);
-
+        return data_peserta::where('competition_id', $this->id)->get();
     }
+
+    public function map($registration) : array {
+        return [
+            $registration->id,
+            $registration->data_peserta['name'],
+            $registration->title,
+            $registration->description,
+            $registration->files['new_name'],
+            $registration->url,
+            $registration->created_at->format('F j, Y h:i a')
+        ];
+    }
+
+    public function headings() : array {
+        return [
+           'ID',
+           'Nama Peserta',
+           'Title',
+           'Deskripsi',
+           'File',
+           'Link Google Drive',
+           'Tanggal Upload'
+        ] ;
+    }
+
+
 }
--- resources/views/pesertaExport.blade.php
+++ resources/views/pesertaExport.blade.php
@@ -1,41 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <table>
-    <thead>
-      <tr>
-      <th>ID</th>
-      <th>Nama Peserta</th>
-      <th>Title</th>
-      <th>Deskripsi</th>
-      <th>File</th>
-      <th>Tanggal Upload</th>
-      </tr>
-    </thead>
-    <tbody>
-    @foreach($data_peserta as $u)
+<table>
+  <thead>
     <tr>
-      <td>{{ $u->id }}</td>
-      <td>{{ $u->data_peserta['name']}}</td>
-      <td>{{ $u->title }}</td>
-      <td>{{ $u->description }}</td>
-      <td>
-        @if ($u->file_id == 0)
-          <a target="_blank" href="{{ $u->url }}">Google Drive</a>
-          @else
-          {{ $u->files['new_name'] }}
-        @endif
-      </td>
-      <td>{{ $u->created_at->format('F j, Y h:i a') }}</td>
+    <th>ID</th>
+    <th>Nama Peserta</th>
+    <th>Title</th>
+    <th>Deskripsi</th>
+    <th>File</th>
+    <th>Tanggal Upload</th>
     </tr>
-    @endforeach
-    </tbody>
-  </table>
-</body>
-</html>
+  </thead>
+  <tbody>
+  @foreach($data_peserta as $u)
+  <tr>
+    <td>{{ $u->id }}</td>
+    <td>{{ $u->data_peserta['name']}}</td>
+    <td>{{ $u->title }}</td>
+    <td>{{ $u->description }}</td>
+    <td>
+      @if ($u->file_id == 0)
+        <a target="_blank" href="{{ $u->url }}">Google Drive</a>
+        @else
+        {{ $u->files['new_name'] }}
+      @endif
+    </td>
+    <td>{{ $u->created_at->format('F j, Y h:i a') }}</td>
+  </tr>
+  @endforeach
+  </tbody>
+</table>
