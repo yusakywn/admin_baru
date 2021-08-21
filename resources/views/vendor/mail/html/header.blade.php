24a19528b40ffe2647e3a1e94391b0cfad4eee78
arthaputra <manager.arthachan@gmail.com>
2021-08-21 14:19:24 +0800

    merger

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Exova Indonesia')
+@if (trim($slot) === 'Exova')
 <img src="https://assets.exova.id/img/logo.png" class="logo" alt="Logo">
 @else
 {{ $slot }}
